f43dba9dc286ee283d62614b79f786d2bb7f56de
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2023-11-22 08:58:14 +0100

    about route

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,14 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    return '<h1> Home Page</h1><a href="/about">go to about page</a>';
+
+});
+
+Route::get('/about', function () {
+    return '<h1> About Page</h1><a href="/">Back Home</a>';
+
 });
