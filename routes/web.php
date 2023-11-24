330fd38f979cd27146de6ea02b2ba31fcb92b542
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2023-11-24 14:29:30 +0100

    html form to db

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\ExampleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ExampleController::class, "homepage"]);
 
 Route::get('/about', [ExampleController::class, "about"]);
+
+Route::post('/register', [UserController::class, "register"]);
