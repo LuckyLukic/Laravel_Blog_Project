d3c99842ff655d9219131351ec0bfbc53a274158
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2023-12-07 10:54:33 +0100

    follow a user

---
+++ app/Http/Controllers/FollowController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Follow;
+use Illuminate\Http\Request;
+
+class FollowController extends Controller {
+    public function createFollow(User $user) {
+
+        if($user->id == auth()->user()->id) {
+            return back()->with('failure', ' you can\'t follow yourself!');
+        }
+
+        $existCheck = Follow::where(['user_id', '=', auth()->user()->id], ['followeduser', '=', $user->id])->count();
+
+        if($existCheck) {
+            return back()->with('failure', 'you are already following that user');
+        }
+
+        $newFollow = new Follow();
+        $newFollow->user_id = auth()->user()->id;
+        $newFollow->followeduser = $user->id;
+        $newFollow->save();
+    }
+
+    public function removeFollow() {
+
+
+    }
+
+
+}
