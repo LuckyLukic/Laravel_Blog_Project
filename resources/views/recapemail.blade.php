88f8bb035b28c8c7d55dd532a17e254f79770b33
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2024-01-03 12:17:23 +0100

    recap email scheduling

---
+++ resources/views/recapemail.blade.php
@@ -0,0 +1,7 @@
+<div style="font-family: sans-serif">
+
+    <p>Here is a quick recap of the application.</p>
+    <p>PostCount: <strong>{{ $postCount }}</strong></p>
+    <p>PostCount: <strong>{{ $userCount }}</strong></p>
+
+</div>
